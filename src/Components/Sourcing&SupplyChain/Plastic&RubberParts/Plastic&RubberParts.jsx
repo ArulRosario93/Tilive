b8ec34c2dde3ad8b599ae205f4f8ac03a248d224
Arul Rosario <itsarrohere380@gmil.com>
2023-05-18 22:37:32 +0530

    Set Set

--- src/Components/Sourcing&SupplyChain/Plastic&RubberParts/Plastic&RubberParts.jsx
+++ src/Components/Sourcing&SupplyChain/Plastic&RubberParts/Plastic&RubberParts.jsx
@@ -3,6 +3,8 @@ import "./Plastic&RubberParts.css";
 import image from "./plastic&rubberParts.jpg";
 import { StartingPage } from "../../Home/StartingPage/StartingPage";
 
+import { TiStarFullOutline } from "react-icons/ti";
+
 const PlasticAndRubberParts = () => {
     return(
         <div className="PlasticAndRubberParts">
@@ -13,6 +15,16 @@ const PlasticAndRubberParts = () => {
         <br />
         <br />
             <img src={image}/>
+        <br />
+        <br />
+        <p>Airsprings for Trailers/CV/Buses  <TiStarFullOutline />  Bushes for Trailers/CV/PV <TiStarFullOutline /> Fenders/Mudguards <TiStarFullOutline /> Headlamps, Tail lamps <TiStarFullOutline /> Rear view mirrors</p>
+        <br />
+        <br />
+        <div className="spaceHEre"></div>
+        {/* <p>Bushes for trailers/CV/PV</p>
+        <p>Fenders/mudguards </p>
+        <p>Headlamps, tail lamps </p>
+        <p>Rear view mirrors </p> */}
         </div>
     )
 }
